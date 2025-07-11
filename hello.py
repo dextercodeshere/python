98d4d81f4fa0df75f103d855b6dec67e4cbf0d95
dextercodeshere <167686619+dextercodeshere@users.noreply.github.com>
2025-07-11 14:09:21 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+    # Integers
+myage = 20
+print(myage)
+
+# Floats
+pivalue = 3.14159
+print(pivalue)
+
+# Strings
+company_name = "GO SOCIAL"
+tagline = "Digital Dominators"
+full_slogan = company_name + ": " + tagline # String concatenation
+print(full_slogan)
+
+# Booleans
+is_active = True
+is_client = False
+print(is_active)
+print(is_client)
+
+# Basic operations
+result_sum = myage + 5
+print("My age plus 5:", result_sum)
+
+greeting = "Hello"
+name = "Sensei"
+full_greeting = greeting + " " + name
+print(full_greeting)
---
+++ restarting.py
@@ -0,0 +1,3 @@
+print("starting python")
+for i in range(1,6):
+    print(f"D {i}: I executed this code")
